06ef2f7ca97306b7be6564e9a083c0a7547b1b68
JimmyTsai55 <159718285+JimmyTsai55@users.noreply.github.com>
2024-03-27 16:14:31 +0800

    2023.03.27 修改
    把類別coint21point 移到新的count.h標頭檔
    並且在計算賭金倍數時 使用記憶體配置來節省電腦運算資源

--- main.cpp
+++ main.cpp
@@ -13,161 +13,17 @@
 #include <cmath>
 #include <string>
 #include <regex>
-
-class Count21Point {
-private:
-    std::vector<int> deck;
-    int RunningCount = 0;
-    
-public:
-    int betmoney = 25;     //一次要下的賭金
-    int poolmoney = 1000;  //總共有多少賭金
-    std::vector<int> A, B, C, D, Player, Banker;
-//             初始化牌堆
-    Count21Point() {
-        initializeDeck();
-    }
-//     製作牌庫 一共五副牌 13*4*5
-    void initializeDeck() {
-        deck.clear();
-        for (int i = 0; i < 5; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                for (int k = 1; k < 14; ++k) {
-                    deck.push_back(k);
-                }
-            }
-        }
-    }
-//    分牌進去想要的牌庫 並從總牌庫裡面刪除
-    void drawCard(std::vector<int> &playerDeck) {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> dis(0, deck.size() - 1);
-        int index = dis(gen);
-        playerDeck.push_back(deck[index]);
-        deck.erase(deck.begin() + index);
-    }
-//    計算你有多少點數
-    int countpoint(const std::vector<int> &decknumber) const{
-        int allpoints = 0;
-        for(int point : decknumber) {
-            if(point >= 2 && point <= 9) {
-                allpoints += point;
-            } else if(point >= 10) {
-                allpoints += 10;
-            }
-        }
-//     如果總數不超過21點則將Ａ認為11點 ， 其餘都將Ａ認為 1 點
-        for (int point : decknumber) {
-            if (point == 1) {
-                allpoints += (allpoints + 11 <= 21) ? 11 : 1;
-            }
-        }
-        return allpoints;
-    }
-    //顯示牌庫剩下多少張牌
-    void showDeck(const std::vector<int> &decknumber) const {
-        for (int num : decknumber) {
-            std::cout << num << " ";
-        }
-    }
-//     顯示所有玩家還有莊家的牌是多少
-    void SHOWALL() {
-        std::cout << "A deck: ";
-        showDeck(A);
-        std::cout << ", " << countpoint(A) << std::endl;
-        std::cout << "B deck: ";
-        showDeck(B);
-        std::cout << ", " << countpoint(B) << std::endl;
-        std::cout << "C deck: ";
-        showDeck(C);
-        std::cout << ", " << countpoint(C) << std::endl;
-        std::cout << "D deck: ";
-        showDeck(D);
-        std::cout << ", " << countpoint(D) << std::endl;
-        std::cout << "Player deck: ";
-        showDeck(Player);
-        std::cout << ", " << countpoint(Player) << std::endl;
-        std::cout << "Bankers deck: ";
-        showDeck(Banker);
-        std::cout << ", " << countpoint(Banker) << std::endl;
-        std::cout << std::endl;
-    }
-//    你要放進去的牌 必且從總牌庫裡面刪除
-    void inputCardNumber(int yourcardnumber) {
-        Player.push_back(yourcardnumber);
-        auto it = std::find(deck.begin(), deck.end(), yourcardnumber);
-        if (it != deck.end()) {
-            deck.erase(it);
-        }
-    }
-    
-    // 展示牌庫  引用private
-    const std::vector<int>& getdeckDeck() const {
-        return deck;
-    }
-//    計算你有多少點數  （使用Hi-Lo算牌法  2-6 總體真數-1  10 J Q K A 總體真數+1
-    int countRunningCount() const {
-        int localRunningCount = 0;
-        for (int card : deck) {
-            if (card >= 2 && card <= 6) {
-                localRunningCount--;
-            } else if (card >= 10 || card == 1) {
-                localRunningCount++;
-            }
-        }
-        return localRunningCount;
-    }
-//     用四捨五入取總牌庫裡面剩下幾副牌
-    float deckRemaining() const {
-        return static_cast<float>(round(deck.size() / 52.0 * 10) / 10);
-    }
-//    清理牌庫
-    void clearDecks() {
-        A.clear();
-        B.clear();
-        C.clear();
-        D.clear();
-        Player.clear();
-        Banker.clear();
-    }
-//    贏了要加多少錢
-    void plusmoney(int& betmultiple) {
-        poolmoney += betmultiple*betmoney;
-    }
-//    輸了要扣多少錢
-    void minusmoney(int& betmultiple) {
-        poolmoney -= betmultiple*betmoney;
-    }
-//    判斷輸入值是否為數字
-    bool isNumber(const std::string &s) const {
-        std::regex pattern("-?[0-9]+");
-        return std::regex_match(s, pattern);
-    }
-//    Hi-Lo計算真數的方程式
-    int trueCount() const {
-        return countRunningCount() / static_cast<int>(deckRemaining());
-    }
-//    Hi-Lo算牌法去計算你應該下多少倍的賭金
-    int bet() const {
-        if(trueCount() != 0) {
-            return trueCount() - 1;
-        } else {
-            return trueCount();
-        }
-    }
-};
-
+#include "Count.h"
 int main() {
     Count21Point c21p;
     std::string inputcommand;
     bool newgame = true;
     bool addstop = true;
-    int betmultiple;
+    int *betmultiple = new int;
     while (newgame) {
         std::cout << "Recommend Bet* : " << c21p.bet() << std::endl;
         std::cout << "How many bet: ";
-        std::cin >> betmultiple;
+        std::cin >> *betmultiple;
 //        if (c21p.isNumber(betmultiple)) {
 //            int betmultiple = std::stoi(betmultiple);
 //        }
@@ -235,6 +91,7 @@ int main() {
                 }
             }
         }
+        delete betmultiple;
     }
     std::cout << "RunningCount : " << c21p.countRunningCount() << std::endl;
     std::cout <<
@@ -247,4 +104,5 @@ int main() {
     
     return 0;
 }
+
 
